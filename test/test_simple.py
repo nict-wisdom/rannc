8493dfe2af6a7c35b77dbe599416550b0150e9d6
Masahiro Tanaka <mtnk@nict.go.jp>
2021-08-02 10:01:54 +0900

    Updated test settings

--- test/test_simple.py
+++ test/test_simple.py
@@ -4,7 +4,7 @@ from . import common, models
 # from . import native_models
 
 default_vals = {
-    "rtol": 1e-2,
+    "rtol": 1e-1,
     "atol": 0,
     "get_dataset": None,
     "loss_out": False
@@ -18,23 +18,21 @@ test_models = [
     {"model": models.OneOpModel},
     {"model": models.TensorMulModel},
     {"model": models.EmbeddingModel, "rtol": 1e-1, "get_dataset": models.EmbeddingModel.get_dataset},
-    {"model": models.FunctionModel, "get_dataset": models.FunctionModel.get_dataset, "rtol": 1e-1},
+    {"model": models.FunctionModel, "get_dataset": models.FunctionModel.get_dataset},
     # {"model": native_models.NativeCallModel01}, # compiles module
-    {"model": models.LossOutModel, "loss_out": True, "rtol": 1e-1}
+    {"model": models.LossOutModel, "loss_out": True}
 ]
 
 
 @pytest.mark.parametrize("test_model", test_models)
-@pytest.mark.parametrize("use_amp", [True])
-@pytest.mark.parametrize("allreduce_amp_master_params", [False])
-@pytest.mark.parametrize("enable_zero", [False])
+@pytest.mark.parametrize("use_amp", [False, True])
+@pytest.mark.parametrize("allreduce_amp_master_params", [False, True])
+@pytest.mark.parametrize("enable_zero", [False, True])
 def test_match(init_dist, init_seed, batch_size, iteration, test_model, use_amp, allreduce_amp_master_params, enable_zero):
 
     if enable_zero and (not allreduce_amp_master_params):
         print("allreduce_amp_master_params must be True if enable_zero == True")
         return
-
-    print("use_amp={} enable_zero={} allreduce_amp_master_params={}".format(use_amp, enable_zero, allreduce_amp_master_params))
 
     for k, v in default_vals.items():
         if k not in test_model:
